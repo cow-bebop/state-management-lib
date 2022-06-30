9d25063c62b0a92dae0868c44a9b5ec64af2afff
けお <o.kengo1987@gmail.com>
2022-06-30 18:43:29 +0900

    feat: context基礎

--- Context01/src/pages/_app.tsx
+++ Context01/src/pages/_app.tsx
@@ -1,21 +1,34 @@
 import type { AppProps } from "next/app";
-import { useState } from "react";
+import { createContext, useContext, useState } from "react";
 import { Layout } from "src/components/Layout";
 import { Todo } from "src/types";
 
 const TODOS: Todo[] = [
   { id: 1, text: "foo", isDone: false },
   { id: 2, text: "bar", isDone: true },
-]
+];
+
+export const ThemeContext = createContext("light");
+export const LangContext = createContext("ja");
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const [todos, setTodos] = useState<Todo[]>(TODOS);
+  const [theme, setTheme] = useState("light");
+  const [lang, setLang] = useState("ja");
 
   return (
-    <>
-      <Layout todoCount={todos.length}>
-        <Component {...pageProps} todos={todos} setTodos={setTodos} />
-      </Layout>
-    </>
+    <ThemeContext.Provider value={theme}>
+      <LangContext.Provider value={lang}>
+        <button
+          onClick={() => {
+            setTheme(prev => prev === "dark" ? "light" : "dark");
+            setLang(prev => prev === "ja" ? "en" : "ja");
+          }}
+        >切り替え</button>
+        <Layout todoCount={todos.length}>
+          <Component {...pageProps} todos={todos} setTodos={setTodos} />
+        </Layout>
+      </LangContext.Provider>
+    </ThemeContext.Provider>
   );
 }
